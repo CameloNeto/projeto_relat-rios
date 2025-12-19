3335685955b72901b5c401b1eaeb3498768d95a2
José Neto <netoalvescamelo@gmail.com>
2025-12-19 14:22:50 -0300

    função de coleta de clientes

--- colect_clients.py
+++ colect_clients.py
@@ -3,6 +3,7 @@ import httpx
 import asyncio
 from keys import DG_TOKEN
 from DataBase import DataBase
+import json
 
 async def colect_clients():
     urlDG = 'https://gestao.dg.energy/api/v1/entities/'
@@ -17,7 +18,8 @@ async def colect_clients():
         request = await httpx.AsyncClient(timeout=120).request("GET", urlDG, headers=headersDG)
         results = request.json().get("results")
         for client in results:
-            database.cur.execute("insert into clients ")
+            print(client)
+            database.insert_clients([client.get('id'), client.get('name'), client.get('document_type'), client.get('document'), json.dumps(client.get('emails')), json.dumps([])])
             break
         break
 
