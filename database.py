30d7c3fca3c77a9f250214e225bae86b8451561e
José Neto <netoalvescamelo@gmail.com>
2025-12-19 10:04:38 -0300

    project keys for env

--- database.py
+++ database.py
@@ -1,7 +1,15 @@
 import sqlite3
 from uuid import uuid4
+import httpx
 
-con = sqlite3.connect('database.db')
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS clients (id integer, name text, document_type text check(document_type in ('CPF', 'CNPJ')) not null, document text not null, emails text, facilities text);")
-con.commit()
+# con = sqlite3.connect('database.db')
+# cur = con.cursor()
+# cur.execute("CREATE TABLE IF NOT EXISTS clients (id integer, name text, document_type text check(document_type in ('CPF', 'CNPJ')) not null, document text not null, emails text, facilities text);")
+# con.commit()
+
+urlDG = 'https://gestao.dg.energy/api/v1/installations/'
+
+headersDG = {
+        'Authorization':f'Token {DG_TOKEN}'
+    }
+
---
+++ keys.py
@@ -0,0 +1,7 @@
+import dotenv
+
+dotenv.load_dotenv()
+
+DG_TOKEN = dotenv.get_key()
+NEOFIN_API_KEY = dotenv.get_key()
+NEOFIN_SECRET_KEY = dotenv.get_key()
